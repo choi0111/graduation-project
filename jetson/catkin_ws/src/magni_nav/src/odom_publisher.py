a66dc6905adb9b03b1b59a419995585ae4d28f17
choi0111 <jinho010111@naver.com>
2026-06-26 17:50:23 +0900

    Restore odometry and lidar TF baseline

--- jetson/catkin_ws/src/magni_nav/src/odom_publisher.py
+++ jetson/catkin_ws/src/magni_nav/src/odom_publisher.py
@@ -1,134 +1,107 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import math
-
 import rospy
+import math
 import tf
-from geometry_msgs.msg import Point, Point32, Pose, Quaternion, Twist, Vector3
 from nav_msgs.msg import Odometry
-
-
-def normalize_angle(angle):
-    return math.atan2(math.sin(angle), math.cos(angle))
-
+from geometry_msgs.msg import Point, Pose, Quaternion, Twist, Vector3, Point32
 
 class OdomPublisher:
     def __init__(self):
-        rospy.init_node("odom_publisher")
+        rospy.init_node('odom_publisher')
 
-        self.left_ticks_per_rev = rospy.get_param("~left_ticks_per_rev", 3788.0)
-        self.right_ticks_per_rev = rospy.get_param("~right_ticks_per_rev", 3691.0)
-        self.wheel_diameter = rospy.get_param("~wheel_diameter", 0.125)
-        self.wheel_base = rospy.get_param("~wheel_base", 0.695)
-        self.left_tick_sign = rospy.get_param("~left_tick_sign", 1.0)
-        self.right_tick_sign = rospy.get_param("~right_tick_sign", 1.0)
+        # STM32와 맞춘 틱 수치
+        self.left_ticks_per_rev = 3788.0
+        self.right_ticks_per_rev = 3691.0
 
-        self.odom_frame = rospy.get_param("~odom_frame", "odom")
-        self.base_frame = rospy.get_param("~base_frame", "base_footprint")
-        self.publish_tf = rospy.get_param("~publish_tf", True)
-        self.rate_hz = rospy.get_param("~rate", 30.0)
-
-        self.wheel_radius = self.wheel_diameter / 2.0
+        self.wheel_radius = 0.125 / 2.0
+        self.wheel_base = 0.695
 
         self.left_ticks = 0
         self.right_ticks = 0
         self.last_left_ticks = 0
         self.last_right_ticks = 0
-        self.have_ticks = False
-        self.first_update = True
 
         self.x = 0.0
         self.y = 0.0
         self.th = 0.0
+
         self.last_time = rospy.Time.now()
+        self.first_run = True
 
-        self.odom_pub = rospy.Publisher("odom", Odometry, queue_size=10)
+        self.odom_pub = rospy.Publisher("odom", Odometry, queue_size=50)
         self.odom_broadcaster = tf.TransformBroadcaster()
-        rospy.Subscriber("wheel_ticks", Point32, self.ticks_callback, queue_size=10)
 
-        rospy.loginfo(
-            "odom_publisher: left_ticks_per_rev=%.3f right_ticks_per_rev=%.3f "
-            "wheel_base=%.3f wheel_diameter=%.3f",
-            self.left_ticks_per_rev,
-            self.right_ticks_per_rev,
-            self.wheel_base,
-            self.wheel_diameter,
-        )
+        # [핵심] STM32가 쏘는 'wheel_ticks (Point32)'를 받도록 세팅!
+        rospy.Subscriber("wheel_ticks", Point32, self.ticks_callback)
+
+        self.rate = rospy.Rate(20)
 
     def ticks_callback(self, msg):
-        self.left_ticks = int(msg.x * self.left_tick_sign)
-        self.right_ticks = int(msg.y * self.right_tick_sign)
-        self.have_ticks = True
+        # x에 담긴 왼쪽 틱, y에 담긴 오른쪽 틱을 꺼내옵니다.
+        self.left_ticks = int(msg.x)
+        self.right_ticks = int(msg.y)
 
     def update(self):
         current_time = rospy.Time.now()
-
-        if not self.have_ticks:
-            self.last_time = current_time
-            return
-
-        if self.first_update:
+        if self.first_run:
             self.last_left_ticks = self.left_ticks
             self.last_right_ticks = self.right_ticks
             self.last_time = current_time
-            self.first_update = False
+            self.first_run = False
             return
 
         dt = (current_time - self.last_time).to_sec()
-        if dt <= 0.0:
+        if dt == 0:
             return
 
         delta_left_ticks = self.left_ticks - self.last_left_ticks
         delta_right_ticks = self.right_ticks - self.last_right_ticks
 
-        distance_left = (
-            float(delta_left_ticks)
-            / self.left_ticks_per_rev
-            * (2.0 * math.pi * self.wheel_radius)
-        )
-        distance_right = (
-            float(delta_right_ticks)
-            / self.right_ticks_per_rev
-            * (2.0 * math.pi * self.wheel_radius)
-        )
+        # 16-bit 타이머 오버플로우 방지
+        if delta_left_ticks > 32768: delta_left_ticks -= 65536
+        elif delta_left_ticks < -32768: delta_left_ticks += 65536
+        if delta_right_ticks > 32768: delta_right_ticks -= 65536
+        elif delta_right_ticks < -32768: delta_right_ticks += 65536
 
+        # 이동 거리 계산
+        distance_left = (delta_left_ticks / self.left_ticks_per_rev) * (2 * math.pi * self.wheel_radius)
+        distance_right = (delta_right_ticks / self.right_ticks_per_rev) * (2 * math.pi * self.wheel_radius)
+
+        # 차동 구동 역학 공식
         distance = (distance_right + distance_left) / 2.0
         d_th = (distance_right - distance_left) / self.wheel_base
 
-        heading_mid = self.th + d_th / 2.0
-        self.x += distance * math.cos(heading_mid)
-        self.y += distance * math.sin(heading_mid)
-        self.th = normalize_angle(self.th + d_th)
+        delta_x = distance * math.cos(self.th)
+        delta_y = distance * math.sin(self.th)
+        delta_th = d_th
+
+        self.x += delta_x
+        self.y += delta_y
+        self.th += delta_th
 
         vx = distance / dt
         vth = d_th / dt
 
-        odom_quat = tf.transformations.quaternion_from_euler(0.0, 0.0, self.th)
+        # TF 발행
+        odom_quat = tf.transformations.quaternion_from_euler(0, 0, self.th)
+        self.odom_broadcaster.sendTransform(
+            (self.x, self.y, 0.),
+            odom_quat,
+            current_time,
+            "base_footprint",
+            "odom"
+        )
 
-        if self.publish_tf:
-            self.odom_broadcaster.sendTransform(
-                (self.x, self.y, 0.0),
-                odom_quat,
-                current_time,
-                self.base_frame,
-                self.odom_frame,
-            )
-
+        # Odom 발행
         odom = Odometry()
         odom.header.stamp = current_time
-        odom.header.frame_id = self.odom_frame
-        odom.child_frame_id = self.base_frame
-        odom.pose.pose = Pose(Point(self.x, self.y, 0.0), Quaternion(*odom_quat))
-        odom.twist.twist = Twist(Vector3(vx, 0.0, 0.0), Vector3(0.0, 0.0, vth))
+        odom.header.frame_id = "odom"
+        odom.child_frame_id = "base_footprint"
 
-        # Nonzero covariance helps AMCL and RViz understand this is wheel odom,
-        # not a perfect pose measurement.
-        odom.pose.covariance[0] = 0.02
-        odom.pose.covariance[7] = 0.02
-        odom.pose.covariance[35] = 0.05
-        odom.twist.covariance[0] = 0.02
-        odom.twist.covariance[35] = 0.05
+        odom.pose.pose = Pose(Point(self.x, self.y, 0.), Quaternion(*odom_quat))
+        odom.twist.twist = Twist(Vector3(vx, 0, 0), Vector3(0, 0, vth))
 
         self.odom_pub.publish(odom)
 
@@ -137,14 +110,13 @@ class OdomPublisher:
         self.last_time = current_time
 
     def spin(self):
-        rate = rospy.Rate(self.rate_hz)
         while not rospy.is_shutdown():
             self.update()
-            rate.sleep()
+            self.rate.sleep()
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     try:
-        OdomPublisher().spin()
+        odom = OdomPublisher()
+        odom.spin()
     except rospy.ROSInterruptException:
         pass
