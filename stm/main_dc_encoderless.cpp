2bb2c828f6fa2886eeb66abb842ac0b044b51f1c
choi0111 <jinho010111@naver.com>
2026-07-10 20:28:54 +0900

    Remove duplicate STM UART MSP init

--- stm/main_dc_encoderless.cpp
+++ stm/main_dc_encoderless.cpp
@@ -169,7 +169,6 @@ static void MX_TIM3_Init(void);
 static void MX_USART2_UART_Init(void);
 static void MX_TIM2_Init(void);
 static void MX_TIM4_Init(void);
-void HAL_UART_MspInit(UART_HandleTypeDef* uartHandle);
 
 /* USER CODE BEGIN PFP */
 static float clamp_float(float value, float min_value, float max_value);
@@ -899,29 +898,6 @@ static void MX_USART2_UART_Init(void)
   if (HAL_UART_Init(&huart2) != HAL_OK)
   {
     Error_Handler();
-  }
-}
-
-void HAL_UART_MspInit(UART_HandleTypeDef* uartHandle)
-{
-  GPIO_InitTypeDef GPIO_InitStruct = {0};
-
-  if (uartHandle->Instance == USART2)
-  {
-    __HAL_RCC_USART2_CLK_ENABLE();
-    __HAL_RCC_GPIOA_CLK_ENABLE();
-
-    /*
-     * NUCLEO/STLink VCP wiring:
-     * PA2 = USART2_TX
-     * PA3 = USART2_RX
-     */
-    GPIO_InitStruct.Pin = GPIO_PIN_2 | GPIO_PIN_3;
-    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
-    GPIO_InitStruct.Pull = GPIO_PULLUP;
-    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
-    GPIO_InitStruct.Alternate = GPIO_AF7_USART2;
-    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
   }
 }
 
