3afc832228ce516ab2273c60f65dcee16b1fadc4
choi0111 <jinho010111@naver.com>
2026-07-27 14:40:44 +0900

    Disambiguate corridor and doorway widths

--- jetson/catkin_ws/src/magni_nav/scripts/corridor_centering.py
+++ jetson/catkin_ws/src/magni_nav/scripts/corridor_centering.py
@@ -269,12 +269,19 @@ class CorridorCentering(object):
             current_width = None
             if left is not None and right is not None:
                 current_width = left + right
+            normal_width_error = None
+            door_width_error = None
+            if current_width is not None:
+                normal_width_error = abs(
+                    current_width - nominal_width)
+                door_width_error = abs(
+                    current_width -
+                    (nominal_width + self.measured_door_recess_depth))
             door_recess_geometry = (
                 current_width is not None and
-                abs(current_width -
-                    (nominal_width +
-                     self.measured_door_recess_depth)) <=
-                self.door_recess_width_tolerance)
+                door_width_error <= self.door_recess_width_tolerance and
+                door_width_error + self.door_reacquire_score_margin <
+                normal_width_error)
 
             self.safety_left_distance = left_safety
             self.safety_right_distance = right_safety
